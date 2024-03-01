7ffd10daadc5260a727361165c418843b8974523
Cartago <cartago.relaxingcup@gmail.com>
2024-03-02 00:23:57 +0100

    Actualización de estilos de la aplicación y componentes

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import { RouterView } from 'vue-router';
 		<nav>
 			<span
 				@click="$router.push({ name: 'mario' })"
-				class="btn">
+				class="link btn">
 				Mario
 			</span>
 		</nav>
@@ -29,29 +29,44 @@ import { RouterView } from 'vue-router';
 #app {
 	display: grid;
 	grid-template-columns: 300px 1fr;
-
+	background-color: var(--gray-200);
 	width: 100%;
 	height: 100%;
 }
 </style>
 <style scoped>
 aside {
+	display: flex;
 	background-color: var(--gray-700);
 	color: var(--gray-100);
-	padding: 10px;
-	display: flex;
 	flex-direction: column;
 	align-items: center;
 	gap: 20px;
+	box-shadow: 0 0 10px 0 black;
+	header {
+		box-shadow: 0 0 2px 0 black;
+
+		padding: 20px;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		background-color: var(--gray-800);
+	}
+	nav {
+		display: flex;
+		flex-direction: column;
+		gap: 20px;
+	}
 }
 main {
-	background-color: burlywood;
 	display: flex;
 	padding: 20px;
 	justify-content: center;
+	overflow: scroll;
 
 	.curriculum {
 		aspect-ratio: 1.414;
+		/* Size A4 */
 		width: 210mm;
 		height: 297mm;
 		background-color: white;
