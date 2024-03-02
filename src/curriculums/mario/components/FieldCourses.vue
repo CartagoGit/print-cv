15197c6e6e349b998e8afb20e83b9d9f0c0a904c
Cartago <cartago.relaxingcup@gmail.com>
2024-03-02 23:08:50 +0100

    Agregados estilos para los distintos colores de los badges y nuevos cursos en la sección de educación complementaria

--- src/curriculums/mario/components/FieldCourses.vue
+++ src/curriculums/mario/components/FieldCourses.vue
@@ -12,7 +12,7 @@
 				</span>
 				<span
 					v-if="field.isOnline"
-					class="font-regular online badge badge--green">
+					class="font-regular online badge badge--purple">
 					online
 				</span>
 			</span>
@@ -42,18 +42,23 @@ defineProps({
 	display: grid;
 	width: 100%;
 	grid-template-columns: 1fr 60px 56px;
+	grid-template-rows: 1fr;
 	gap: 2px;
-	.date {
-		color: var(--primary);
-		justify-self: end;
-		display: flex;
-		align-items: center;
-		font-size: 13px;
+	.company, .badges, .date {
+		grid-row: 1;
+	}
+	.company {
+		text-wrap: nowrap;
+		grid-column: 1 / 3
 	}
 	.badges {
 		display: flex;
 		gap: 1px;
 		align-items: center;
+		grid-column: 2 / 3;
+		.badge{
+			opacity: 0.8
+		}
 	}
 	.time {
 		min-width: 20px;
@@ -61,6 +66,14 @@ defineProps({
 	}
 	.online {
 		margin-left: auto;
+	}
+	.date {
+		grid-column: 3 / 4;
+		color: var(--primary);
+		justify-self: end;
+		display: flex;
+		align-items: center;
+		font-size: 13px;
 	}
 }
 </style>
